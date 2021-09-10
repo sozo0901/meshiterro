545a107e6dad151cae38750ad62547a3f820b9ee
sozo0901 <taiki.ishikawa.97@gmail.com>
2021-09-10 07:54:27 +0000

    [Add] 投稿一覧画面を作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '20cf12bc5dc13d921346c702535ac9d057d4b3bca6b84965cebd2bdd0e46cdab3e774fea69342b0f7837773bffd0d3ba6c9dbe142514531e6e7ed2f565e7f3ac'
