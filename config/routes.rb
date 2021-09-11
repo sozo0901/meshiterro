5255f300118fc269ebafbd3c5d6696c20fcdcd06
sozo0901 <taiki.ishikawa.97@gmail.com>
2021-09-11 05:46:22 +0000

    [Start] 4-5 マイページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   root to: 'homes#top'
   devise_for :users
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
@@ -6,5 +7,6 @@ Rails.application.routes.draw do
 
     resources :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show]
 
 end
